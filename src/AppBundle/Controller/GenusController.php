dfa5fc6209294b332ff449072d463cd0dbf4184c
saysa <sbo@conamed.de>
2017-12-20 08:47:44 +0100

    simple route with simple assets

---
+++ src/AppBundle/Controller/GenusController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace AppBundle\Controller;
+
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class GenusController extends Controller
+{
+    /**
+     * @Route("/genus/{genusName}")
+     */
+    public function showAction($genusName)
+    {
+        $notes = [
+         'Octopus asked me a riddle, outsmarted me',
+         'I counted 8 legs... as they wrapped around me',
+         'Inked!'
+        ];
+
+        return $this->render('genus/show.html.twig', [
+            "name" => $genusName,
+            "notes" => $notes,
+        ]);
+    }
+}

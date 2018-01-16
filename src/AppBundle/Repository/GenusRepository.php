6d43db58cd3cda59def3842d416a8e4dc6bfa25c
saysa <sbo@conamed.de>
2018-01-16 08:51:10 +0100

    PHP docs : return an array of Genus objects

--- src/AppBundle/Repository/GenusRepository.php
+++ src/AppBundle/Repository/GenusRepository.php
@@ -2,10 +2,14 @@
 
 namespace AppBundle\Repository;
 
+use AppBundle\Entity\Genus;
 use Doctrine\ORM\EntityRepository;
 
 class GenusRepository extends EntityRepository
 {
+    /**
+     * @return Genus[]
+     */
     public function findAllPublishedOrderedBySize()
     {
         return $this->createQueryBuilder('genus')

5edea1a7be1e2b9dae39cf51221280fc5d36a2e1
saysa <sbo@conamed.de>
2018-01-16 08:38:45 +0100

    custom query
    returns a QueryBuilder
    the alias is 'genus' but could be anything else like g

--- src/AppBundle/Repository/GenusRepository.php
+++ src/AppBundle/Repository/GenusRepository.php
@@ -6,5 +6,8 @@ use Doctrine\ORM\EntityRepository;
 
 class GenusRepository extends EntityRepository
 {
-
+    public function findAllPublishedOrderedBySize()
+    {
+        return $this->createQueryBuilder('genus');
+    }
 }

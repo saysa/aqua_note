81e90410e2efd4686f7488f46573d060f5efa0cd
saysa <sbo@conamed.de>
2018-01-16 07:56:22 +0100

    add a isPublished property and its setter

--- src/AppBundle/Entity/Genus.php
+++ src/AppBundle/Entity/Genus.php
@@ -37,6 +37,11 @@ class Genus
      */
     private $funFact;
 
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isPublished = true;
+
 	/**
 	 * @return mixed
 	 */
@@ -116,5 +121,13 @@ class Genus
     public function getUpdatedAt()
     {
         return new \DateTime('-'.rand(0, 100).' days');
+    }
+
+    /**
+     * @param mixed $isPublished
+     */
+    public function setIsPublished($isPublished)
+    {
+        $this->isPublished = $isPublished;
     }
 }

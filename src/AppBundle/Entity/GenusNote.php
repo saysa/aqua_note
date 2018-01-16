bddf266b8fa7bacf3f3cefaf590a2d8a98a35b58
saysa <sbo@conamed.de>
2018-01-16 18:01:19 +0100

    generate ORM Annotation

--- src/AppBundle/Entity/GenusNote.php
+++ src/AppBundle/Entity/GenusNote.php
@@ -10,5 +10,30 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class GenusNote
 {
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
 
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $userAvatarFilename;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $note;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createAt;
 }

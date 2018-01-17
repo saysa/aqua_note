efcaf053740bd3d4f6ac34dd4a8fd4b25275b37a
saysa <sbo@conamed.de>
2018-01-17 10:55:19 +0100

    bring GenusNotes to live

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -79,33 +79,17 @@ class GenusController extends Controller
      */
     public function getNotesAction(Genus $genus)
     {
-        foreach ($genus->getNotes() as $note) {
-            dump($note);
-        }
+        $notes = [];
 
-        $notes = [
-         [
-          'id'        => 1,
-          'username'  => 'AquaPelham',
-          'avatarUri' => '/images/leanna.jpeg',
-          'note'      => 'Octopus asked me a riddle, outsmarted me',
-          'date'      => 'Dec. 10, 2015',
-         ],
-         [
-          'id'        => 2,
-          'username'  => 'AquaWeaver',
-          'avatarUri' => '/images/ryan.jpeg',
-          'note'      => 'I counted 8 legs... as they wrapped around me',
-          'date'      => 'Dec. 1, 2015',
-         ],
-         [
-          'id'        => 3,
-          'username'  => 'AquaPelham',
-          'avatarUri' => '/images/leanna.jpeg',
-          'note'      => 'Inked!',
-          'date'      => 'Aug. 20, 2015',
-         ],
-        ];
+        foreach ($genus->getNotes() as $note) {
+            $notes[] = [
+                'id' => $note->getId(),
+                'username' => $note->getUsername(),
+                'avatarUri' => 'images'.$note->getUserAvatarFilename(),
+                'note' => $note->getNote(),
+                'date' => $note->getCreatedAt()->format('M d, Y'),
+            ];
+        }
 
         $data = [
          'notes' => $notes,

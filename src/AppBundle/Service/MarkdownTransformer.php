5cc869f64d68f500d1a55bed911c8607fd50bf5a
saysa <sbo@conamed.de>
2018-02-06 11:10:23 +0100

    use Type-Hints

--- src/AppBundle/Service/MarkdownTransformer.php
+++ src/AppBundle/Service/MarkdownTransformer.php
@@ -2,17 +2,19 @@
 
 namespace AppBundle\Service;
 
+use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
+
 class MarkdownTransformer
 {
     private $markdownParser;
 
-    public function __construct($markdownParser)
+    public function __construct(MarkdownParserInterface $markdownParser)
     {
         $this->markdownParser = $markdownParser;
     }
 
     public function parse($str)
     {
-        return $funFact = $this->markdownParser->transform($str);
+        return $funFact = $this->markdownParser->transformMarkdown($str);
     }
 }

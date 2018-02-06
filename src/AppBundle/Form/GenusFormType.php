02c9e1228f63d3dddc8c30b63843cdee25779fb2
saysa <sbo@conamed.de>
2018-02-06 18:29:25 +0100

    control how to render the date field

--- src/AppBundle/Form/GenusFormType.php
+++ src/AppBundle/Form/GenusFormType.php
@@ -8,6 +8,7 @@ use AppBundle\Repository\SubFamilyRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -32,7 +33,9 @@ class GenusFormType extends AbstractType
                     'No' => false,
                 ],
             ])
-            ->add('firstDiscoveredAt')
+            ->add('firstDiscoveredAt', DateType::class, [
+                'widget' => 'single_text',
+            ])
         ;
     }
 

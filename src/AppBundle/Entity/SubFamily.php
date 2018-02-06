6eec4819e2a35739a7af92396a6c6fd38faa8890
saysa <sbo@conamed.de>
2018-02-06 14:52:46 +0100

    it needs to be able to turn a SubFamily object into a string so it can render the text for each option in the select.

--- src/AppBundle/Entity/SubFamily.php
+++ src/AppBundle/Entity/SubFamily.php
@@ -21,4 +21,25 @@ class SubFamily
      * @ORM\Column(type="string")
      */
     private $name;
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function __toString()
+    {
+        return $this->getName();
+    }
 }

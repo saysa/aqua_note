ac27353d4d09706b207b81a5e647d968c6ce447c
saysa <sbo@conamed.de>
2018-02-06 14:17:24 +0100

    Thanks to the data_class option, the form creates a new Genus object behind the scenes. And then it sets the data on it.

--- src/AppBundle/Form/GenusFormType.php
+++ src/AppBundle/Form/GenusFormType.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Form;
 
+use AppBundle\Entity\Genus;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -19,7 +20,9 @@ class GenusFormType extends AbstractType
 
     public function configureOptions(OptionsResolver $resolver)
     {
-
+        $resolver->setDefaults([
+            'data_class' => Genus::class
+        ]);
     }
 
     public function getBlockPrefix()

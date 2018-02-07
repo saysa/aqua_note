5e9db031177bec60aa879054b20dcab6ce155bd5
saysa <sbo@conamed.de>
2018-02-07 11:59:23 +0100

    add validation constraint with options

--- src/AppBundle/Entity/Genus.php
+++ src/AppBundle/Entity/Genus.php
@@ -4,6 +4,7 @@ namespace AppBundle\Entity;
 
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="AppBundle\Repository\GenusRepository")
@@ -26,17 +27,21 @@ class Genus
 
 	/**
 	 * @ORM\Column(type="string")
+     * @Assert\NotBlank()
 	 */
 	private $name;
 
     /**
      * @ORM\ManyToOne(targetEntity="AppBundle\Entity\SubFamily")
      * @ORM\JoinColumn(nullable=false)
+     * @Assert\NotBlank()
      */
     private $subFamily;
 
     /**
      * @ORM\Column(type="integer")
+     * @Assert\NotBlank()
+     * @Assert\Range(min="0", minMessage="Negative species! Come on...")
      */
     private $speciesCount;
 
@@ -52,6 +57,7 @@ class Genus
 
     /**
      * @ORM\Column(type="date")
+     * @Assert\NotBlank()
      */
     private $firstDiscoveredAt;
 

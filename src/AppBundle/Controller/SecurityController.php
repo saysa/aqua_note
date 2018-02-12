3ce90240dd129f59746c0c15a0bd8a28adaac1dd
saysa <sbo@conamed.de>
2018-02-12 18:17:44 +0100

    add SecurityController

---
+++ src/AppBundle/Controller/SecurityController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class SecurityController extends Controller
+{
+    /**
+     * @Route("/login", name="security_login")
+     */
+    public function loginAction()
+    {
+
+    }
+}

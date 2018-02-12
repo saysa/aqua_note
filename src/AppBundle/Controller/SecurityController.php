452ba3cfb8eab446b2cc3b9b2086560f2f8e3c64
saysa <sbo@conamed.de>
2018-02-12 18:28:51 +0100

    add the Login Controller

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -12,6 +12,20 @@ class SecurityController extends Controller
      */
     public function loginAction()
     {
+        $authUtils = $this->get('security.authentication_utils');
 
+        // get the login error if there is one
+        $error = $authUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authUtils->getLastUsername();
+
+        return $this->render(
+            'security/login.html.twig',
+            array(
+                'last_username' => $lastUsername,
+                'error'         => $error,
+            )
+        );
     }
 }

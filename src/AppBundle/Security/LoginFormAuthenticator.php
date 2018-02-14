7ca644cc45dfa8990389c2dd207baf7647d92ff8
saysa <sbo@conamed.de>
2018-02-14 10:25:04 +0100

    the form is not pre-filled with the email address I just used.
    the login form is automatically setting the authentication error to the session for us. But, it is not setting the last username on the session

--- src/AppBundle/Security/LoginFormAuthenticator.php
+++ src/AppBundle/Security/LoginFormAuthenticator.php
@@ -10,6 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Guard\Authenticator\AbstractFormLoginAuthenticator;
@@ -77,6 +78,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
         $form->handleRequest($request);
 
         $data = $form->getData();
+        $request->getSession()->set(Security::LAST_USERNAME, $data['_username']);
 
         return $data;
     }

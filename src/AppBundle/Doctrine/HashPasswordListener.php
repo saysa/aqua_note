42b486f8bb78eff15e3e35b91483a5156d96c18a
saysa <sbo@conamed.de>
2018-02-14 11:18:09 +0100

    Symfony comes with a built-in service that's really good at encoding passwords.
    It's called security.password_encoder and if you looked it up on debug:container, its class is UserPasswordEncoder.

--- src/AppBundle/Doctrine/HashPasswordListener.php
+++ src/AppBundle/Doctrine/HashPasswordListener.php
@@ -5,9 +5,16 @@ namespace AppBundle\Doctrine;
 use AppBundle\Entity\User;
 use Doctrine\Common\EventSubscriber;
 use Doctrine\ORM\Event\LifecycleEventArgs;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoder;
 
 class HashPasswordListener implements EventSubscriber
 {
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoder $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
 
     /**
      * Returns an array of events this subscriber wants to listen to.

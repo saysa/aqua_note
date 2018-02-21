14edb3f3384329c2869e393b2b05d5ef5b5466a8
saysa <sbo@conamed.de>
2018-02-21 17:04:12 +0100

    add an email field

--- src/AppBundle/Form/UserRegistrationForm.php
+++ src/AppBundle/Form/UserRegistrationForm.php
@@ -4,6 +4,7 @@ namespace AppBundle\Form;
 
 use AppBundle\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -11,7 +12,9 @@ class UserRegistrationForm extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-
+        $builder
+            ->add('email', EmailType::class)
+            ;
     }
 
     public function configureOptions(OptionsResolver $resolver)

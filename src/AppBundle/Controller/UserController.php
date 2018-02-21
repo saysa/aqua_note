b50bfe316e69498e92b62a5c6e530a38b0383323
saysa <sbo@conamed.de>
2018-02-21 18:35:09 +0100

    Automatically Login after Registration!
    This is always pretty easy, but it's especially easy because we're using Guard authentication.
    it redirected us back to the URL we were trying to visit before we were sent to the login page.
    This is really great because it means that if your user tries to access a secure page - like your checkout form - they'll end up back on the checkout form after registration. Then they can keep buying your cool stuff.

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -30,7 +30,14 @@ class UserController extends Controller
 
             $this->addFlash('success', 'Welcome '.$user->getEmail());
 
-            return $this->redirectToRoute('homepage');
+            return $this->get('security.authentication.guard_handler')
+                ->authenticateUserAndHandleSuccess(
+                    $user,
+                    $request,
+                    $this->get('app.security.login_form_authenticator'),
+                    'main'
+                )
+                ;
         }
 
         return $this->render('user/register.html.twig', [

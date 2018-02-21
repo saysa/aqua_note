86c77a15c0b2ea1984e89dcf73aa135966a4575f
saysa <sbo@conamed.de>
2018-02-21 17:01:54 +0100

    bind the form to User

--- src/AppBundle/Form/UserRegistrationForm.php
+++ src/AppBundle/Form/UserRegistrationForm.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Form;
 
+use AppBundle\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -15,7 +16,9 @@ class UserRegistrationForm extends AbstractType
 
     public function configureOptions(OptionsResolver $resolver)
     {
-
+        $resolver->setDefaults([
+            'data_class' => User::class
+        ]);
     }
 
     public function getBlockPrefix()

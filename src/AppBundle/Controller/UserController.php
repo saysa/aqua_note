bb439f702098c29a5a6e3e424cee81a057087675
saysa <sbo@conamed.de>
2018-02-21 17:16:36 +0100

    rendering the Register Form

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -2,9 +2,25 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Form\UserRegistrationForm;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class UserController extends Controller
 {
+    /**
+     * @Route("/register", name="user_register")
+     * @param Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function registerAction(Request $request)
+    {
+        $form = $this->createForm(UserRegistrationForm::class);
 
+        return $this->render('user/register.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
 }

d1674742facfd73befc3665830052a34b02af5eb
saysa <sbo@conamed.de>
2018-02-21 17:00:55 +0100

    create a new RegistrationForm

---
+++ src/AppBundle/Form/UserRegistrationForm.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserRegistrationForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'app_bundle_user_registration_form';
+    }
+}

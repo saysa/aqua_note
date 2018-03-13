ceb3ed4264f76cf7d3ba5b2e27452368e2d28e4f
saysa <sbo@conamed.de>
2018-03-13 17:14:05 +0100

    Using Class Service ids
    When your service id is your class name, you can actually remove the class key to shorten things.
    But when we did that... we broke our app! Any code referencing the old service id will explode!
    Create a new file in config called legacy_aliases.yml. Inside, add the normal services key, then paste the old service id set to @. Copy the new service id - the class name - and paste it: @AppBundle\Service\MarkdownTransformer
    This creates something called a service alias. This is not a new feature, though this shorter syntax is. A service alias is like a symbolic link: whenever some code asks for the app.markdown_transformer service, the AppBundle\Service\MarkdownTransformer service will be returned. This will keep our old code working with no effort.
    
    To load that file, at the top of services.yml, add an imports key with resource set to legacy_aliases.yml

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -52,7 +52,7 @@ class GenusController extends Controller
             throw $this->createNotFoundException('genus not found');
         }
 
-        $markdownParser = $this->get(MarkdownTransformer::class);
+        $markdownParser = $this->get('app.markdown_transformer');
         $funFact = $markdownParser->parse($genus->getFunFact());
 
         $recentNotes = $em->getRepository('AppBundle:GenusNote')->findAllRecentNotesForGenus($genus);
